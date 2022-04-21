9e15a7a87315e87e3e5ccca17151b3f5382e2c21
Péter Diviánszky <divipp@gmail.com>
2022-04-21 09:33:55 +0200

    fix rebase

--- onchain/src/Hello.hs
+++ onchain/src/Hello.hs
@@ -17,7 +17,9 @@ validator = plam $ \dn dunit dsc -> validator' # pfromData dn # pfromData dunit 
 -- test shareing newtypes/datatypes with apps
 
 validator' :: ClosedTerm (PInteger :--> PUnit :--> PScriptContext :--> PUnit)
-validator' = plam $ \n _unit sc -> helloLogic # n # unTermCont (getContinuingDatum sc)
+validator' = plam $ \n _unit sc -> unTermCont $ do
+  datum <- pgetContinuingDatum @PInteger sc
+  pure $ helloLogic # n # pfromData datum
 
 helloLogic :: ClosedTerm (PInteger :--> PInteger :--> PUnit)
 helloLogic = plam $ \n m -> unTermCont $ passert "int was not correct" $ n + 1 #== m
