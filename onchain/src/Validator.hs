2d57c985dbe4f712e45618a5803864d1c6db6140
Geometer1729 <16kuhnb@gmail.com>
2022-05-09 12:15:26 -0400

    pair with fraser refactor validator

--- onchain/src/Validator.hs
+++ onchain/src/Validator.hs
@@ -1,71 +1,101 @@
-{-# OPTIONS_GHC -Wno-orphans #-}
-
 module Validator (
   ConfigurationDatum,
-  configUpdateValidator,
-  mintValidator,
+  nftMintingPolicy,
+  nftCS,
+  mainValidator,
+  mainAddress,
 ) where
 
 import Plutarch
 
 import Plutarch.Api.V1
+import Plutarch.Builtin (ppairDataBuiltin)
+import Plutarch.Extensions.List (unsingleton)
 import Plutarch.Extensions.Monad
 import Plutarch.Extra.Api
 import Plutarch.Extra.TermCont
 import Plutarch.Prelude
 import Plutarch.Unsafe
+import Plutus.V1.Ledger.Api
 
 type ConfigurationDatum :: PType
 type ConfigurationDatum = PBuiltinList PCurrencySymbol
 
-configUpdateValidator :: Term s (PAsData PUnit :--> PAsData PUnit :--> PAsData PScriptContext :--> PUnit)
-configUpdateValidator = signedByValidator magicPKH
-
-magicPKH :: Term s PPubKeyHash
-magicPKH = pconstant "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+-- TODO move this to an initial config thing
+_magicPKH :: Term s PPubKeyHash
+_magicPKH = pconstant "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
 -- TODO real key
 
-signedByValidator :: Term s PPubKeyHash -> Term s (PAsData PUnit :--> PAsData PUnit :--> PAsData PScriptContext :--> PUnit)
-signedByValidator key = plam $ \_ _ sc -> unTermCont $ do
+_signedByValidator :: Term s PPubKeyHash -> Term s (PAsData PUnit :--> PAsData PUnit :--> PAsData PScriptContext :--> PUnit)
+_signedByValidator key = plam $ \_ _ sc -> unTermCont $ do
   PScriptContext sc' <- pmatchC $ pfromData sc
   txinfo <- pletFieldC @"txInfo" sc'
   signatures <- pletFieldC @"signatories" txinfo
   pguardC "not signed by key" $ pelem # key #$ pmap # plam pfromData # signatures
   pure $ pcon PUnit
 
-configID :: Term s PTxId
-configID = pconstant "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+nftMintingPolicy :: Term s PTxOutRef -> Term s (PData :--> PScriptContext :--> POpaque)
+nftMintingPolicy outRef = plam $ \_ sc' -> unTermCont $ do
+  PScriptContext sc <- pmatchC sc'
+  scRec <- pletFieldsC @'["txInfo", "purpose"] sc
+  rec <- pletFieldsC @'["mint", "inputs"] $ getField @"txInfo" scRec
+  pguardC "didn't spend outRef" $ pelem # outRef #$ (pmap # pfield @"outRef" #$ pfromData (getField @"inputs" rec))
+  PMinting csData <- pmatchC $ getField @"purpose" scRec
+  let cs = pfield @"_0" # csData
+  PValue m <- pmatchC $ pfromData (getField @"mint" rec)
+  PMap l <- pmatchC m
+  let h = unsingleton l
+  pguardC "minting wrong currency symobol" $ pfstBuiltin # h #== cs
+  let tokenMap = psndBuiltin # h
+  PMap tokenList <- pmatchC $ pfromData tokenMap
+  let singleToken = unsingleton tokenList
+  pguardC "minted not exactly one" $ psndBuiltin # singleToken #== pdata 1
+  pure $ popaque (pcon PUnit)
 
--- TODO I think this should just be the validatorHash?
+nftCS :: ClosedTerm PTxOutRef -> CurrencySymbol
+nftCS outRef = mintingPolicySymbol $ mkMintingPolicy $ nftMintingPolicy outRef
 
-mintValidator :: Term s (PAsData PUnit :--> PAsData PInteger :--> PAsData PScriptContext :--> PUnit)
-mintValidator = plam $ \_ n sc -> unTermCont $ do
-  PScriptContext sc' <- pmatchC $ pfromData sc
-  txinfo <- pletFieldC @"txInfo" sc'
-  rec <- pletFieldsC @'["mint", "inputs", "datums"] txinfo
+mainValidator :: ClosedTerm PTxOutRef -> Term s (PData :--> PData :--> PScriptContext :--> POpaque)
+mainValidator outRef = plam $ \_ n' sc' -> unTermCont $ do
+  PScriptContext sc <- pmatchC sc'
+  scRec <- pletFieldsC @'["txInfo", "purpose"] sc
+  rec <- pletFieldsC @'["mint", "inputs", "datums"] $ getField @"txInfo" scRec
   let minting = getField @"mint" rec
-  let inputs = getField @"inputs" rec
+  inputs <- pletC $ pfromData $ getField @"inputs" rec
   let datumTable = getField @"datums" rec
-  -- TODO I'm not sure the next line is correct
-  PJust configInput <- pmatchC $ pfind # plam (\t -> pfromData (pfield @"id" # pfromData (pfield @"outRef" # pfromData t)) #== configID) # pfromData inputs
-  PDJust configDatumHashRecord <- pmatchC $ pfield @"datumHash" #$ pfromData $ pfield @"resolved" # configInput
-  configDatumHash <- pletC $ pfromData $ pfield @"_0" # configDatumHashRecord
-  -- TODO
-  -- using PByteString and unsafeCoerce
-  -- is a temporary fix for PCurrencySymbol and PTokenName not having
-  -- PTryFrom PData instances
-  PJust configAsData <- pmatchC $ pparseDatum @(PBuiltinPair (PAsData PByteString) (PAsData PByteString)) # configDatumHash # datumTable
-  let config = pfromData configAsData
-  let mustMintThis = pfromData $ pelemAt @PBuiltinList # pfromData n # punsafeCoerce config
-  pguardC "indexed token not minted" $ containsPosAmt # mustMintThis # minting
-  pure $ pcon PUnit
+  PSpending spending <- pmatchC $ getField @"purpose" scRec
+  PJust ownInput <- pmatchC $ pfindOwnInput # inputs # (pfield @"_0" # spending)
+  -- TODO check for nft at config input
+  inputResolved <- pletFieldC @"resolved" ownInput
+  -- we require the token to be "" for now
+  pmatchC (containsPosAmt # (ppairDataBuiltin # pdata (pconstant $ nftCS outRef) # pdata (pcon $ PTokenName $ pconstant "")) # (pfield @"value" # inputResolved)) >>= \case
+    PFalse -> do
+      -- TODO to allow more data at this address this should be a check
+      -- that some input has the nft
+      pguardC "no magic nft" $ pcon PFalse
+      pure $ popaque $ pcon PUnit
+    PTrue -> do
+      -- todo it would be better to get this at the same time as the value
+      PDJust configDatumHashRecord <- pmatchC $ pfield @"datumHash" # inputResolved
+      let configDatumHash = pfield @"_0" # configDatumHashRecord
+      -- TODO
+      -- using PByteString and unsafeCoerce
+      -- is a temporary fix for PCurrencySymbol and PTokenName not having
+      -- PTryFrom PData instances
+      PJust configAsData <- pmatchC $ pparseDatum @(PBuiltinPair (PAsData PByteString) (PAsData PByteString)) # configDatumHash # datumTable
+      let config = pfromData configAsData
+      -- ideally this would be ptryFromData but it's not exported
+      let n = pfromData $ punsafeCoerce n'
+      let mustMintThis = pfromData $ pelemAt @PBuiltinList # n # punsafeCoerce config
+      pguardC "indexed token not minted" $ containsPosAmt # mustMintThis # minting
+      pure $ popaque $ pcon PUnit
 
 containsPosAmt :: Term s (PBuiltinPair (PAsData PCurrencySymbol) (PAsData PTokenName) :--> PValue :--> PBool)
 containsPosAmt = plam $ \ac val -> unTermCont $ do
   PValue m <- pmatchC val
   PMap l <- pmatchC m
-  pmatchC (pfind # plam (\p -> pfromData (pfstBuiltin # p) #== pfromData (pfstBuiltin # ac)) # l) >>= \case
+  pmatchC (pfind # plam (\p -> (pfstBuiltin # p) #== (pfstBuiltin # ac)) # l) >>= \case
     PNothing -> pure $ pcon PFalse
     PJust te -> do
       PMap tokens <- pmatchC $ pfromData $ psndBuiltin # te
@@ -73,7 +103,10 @@ containsPosAmt = plam $ \ac val -> unTermCont $ do
         pany
           # plam
             ( \p ->
-                pfromData (pfstBuiltin # p) #== pfromData (psndBuiltin # ac)
+                (pfstBuiltin # p) #== (psndBuiltin # ac)
                   #&& 0 #< pfromData (psndBuiltin # p)
             )
           # tokens
+
+mainAddress :: ClosedTerm PTxOutRef -> ValidatorHash
+mainAddress outRef = validatorHash $ mkValidator (mainValidator outRef)
