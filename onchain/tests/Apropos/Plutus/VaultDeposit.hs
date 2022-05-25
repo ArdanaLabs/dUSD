19d4c2c6d7e449aff2dd2c65c75af9cc6833a5d8
Geometer1729 <16kuhnb@gmail.com>
2022-05-25 16:32:36 -0400

    start vault deposit model

---
+++ onchain/tests/Apropos/Plutus/VaultDeposit.hs
@@ -0,0 +1,32 @@
+module Apropos.Plutus.VaultDeposit (
+  VaultDepProp(..),
+  VaultDepModel(..),
+) where
+
+import Apropos
+import Plutus.V1.Ledger.Api
+import Plutus.V1.Ledger.Value (assetClassValueOf)
+import Apropos.Plutus.AssetClass (ada)
+
+data VaultDepProp
+  = AdaDecreased
+  | DatumChanged
+  deriving stock (Eq,Ord,Show,Generic)
+  deriving anyclass (Enumerable,Hashable)
+
+data VaultDepModel
+  = VaultDepModel
+    { input :: (Address,Value,Maybe Datum)
+    , output :: (Address,Value,Maybe Datum)
+    }
+
+instance LogicalModel VaultDepProp where
+  logic = Yes
+
+instance HasLogicalModel VaultDepProp VaultDepModel where
+  satisfiesProperty AdaDecreased VaultDepModel{input=(_,inVal,_),output=(_,outVal,_)} =
+    assetClassValueOf outVal ada < assetClassValueOf inVal ada
+  satisfiesProperty DatumChanged VaultDepModel{input=(_,_,inDatum),output=(_,_,outDatum)} =
+    inDatum /= outDatum
+
+
