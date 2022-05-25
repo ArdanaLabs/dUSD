1fc3dd7a9014578955916a35c8b91163f1f2f280
Geometer1729 <16kuhnb@gmail.com>
2022-05-25 16:38:11 -0400

    ignore envrc fix format

--- onchain/tests/Apropos/Plutus/VaultDeposit.hs
+++ onchain/tests/Apropos/Plutus/VaultDeposit.hs
@@ -1,32 +1,29 @@
 module Apropos.Plutus.VaultDeposit (
-  VaultDepProp(..),
-  VaultDepModel(..),
+  VaultDepProp (..),
+  VaultDepModel (..),
 ) where
 
 import Apropos
+import Apropos.Plutus.AssetClass (ada)
 import Plutus.V1.Ledger.Api
 import Plutus.V1.Ledger.Value (assetClassValueOf)
-import Apropos.Plutus.AssetClass (ada)
 
 data VaultDepProp
   = AdaDecreased
   | DatumChanged
-  deriving stock (Eq,Ord,Show,Generic)
-  deriving anyclass (Enumerable,Hashable)
+  deriving stock (Eq, Ord, Show, Generic)
+  deriving anyclass (Enumerable, Hashable)
 
-data VaultDepModel
-  = VaultDepModel
-    { input :: (Address,Value,Maybe Datum)
-    , output :: (Address,Value,Maybe Datum)
-    }
+data VaultDepModel = VaultDepModel
+  { input :: (Address, Value, Maybe Datum)
+  , output :: (Address, Value, Maybe Datum)
+  }
 
 instance LogicalModel VaultDepProp where
   logic = Yes
 
 instance HasLogicalModel VaultDepProp VaultDepModel where
-  satisfiesProperty AdaDecreased VaultDepModel{input=(_,inVal,_),output=(_,outVal,_)} =
+  satisfiesProperty AdaDecreased VaultDepModel {input = (_, inVal, _), output = (_, outVal, _)} =
     assetClassValueOf outVal ada < assetClassValueOf inVal ada
-  satisfiesProperty DatumChanged VaultDepModel{input=(_,_,inDatum),output=(_,_,outDatum)} =
+  satisfiesProperty DatumChanged VaultDepModel {input = (_, _, inDatum), output = (_, _, outDatum)} =
     inDatum /= outDatum
-
-
