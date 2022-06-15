22a4e249949f50e0a1a24aea32ebf936ec13ec9f
Evan Piro <evan.piro@platonic.systems>
2022-06-15 14:13:22 -0400

    offchain-ctl/flake-module.nix: added ctl blockFetcher config

--- offchain-ctl/flake-module.nix
+++ offchain-ctl/flake-module.nix
@@ -15,6 +15,12 @@
         sources = [ "hello-world-api/src" ];
         main = "Main";
       };
+      config = {
+        datumCache.blockFetcher.firstBlock = {
+          slot = 60854917;
+          id = "1c157a01120772c35d468b425e6ef228d5b6cec5977f7897540aa8d0870f9ab9";
+        };
+      };
     in
     {
       packages = {
@@ -22,7 +28,7 @@
         ctl-scaffold-runtime = pkgs.buildCtlRuntime { };
       };
       apps = {
-        ctl-scaffold-runtime = pkgs.launchCtlRuntime { };
+        ctl-scaffold-runtime = pkgs.launchCtlRuntime config;
       };
       devShells.offchain-ctl = offchainPsProject.devShell;
     };
