b15287129cd079d5abf64f70645b931f8e92d5c4
Geometer1729 <16kuhnb@gmail.com>
2022-06-16 13:39:57 -0400

    improve wait function

--- offchain-ctl/hello-world-api/src/Main.purs
+++ offchain-ctl/hello-world-api/src/Main.purs
@@ -63,29 +63,41 @@ main = launchAff_ $ do
     txId <- payToHello 5 vhash
     logInfo' $ "Woooo! You did a CTL transaction. Id: " <> show txId
 
-    countToZeroOrTx 60 vhash txId
+    -- There's gotta be a way to do this with MaybeT Eff or something
+    -- but I'm not sure yet
+    maybeTxin <- waitForTx 60 vhash txId
 
-    logInfo' "Try to increment"
-    maybeNewId <- incHello 6 vhash validator txId
-    case maybeNewId of
-      Just txId2 -> do
-        countToZeroOrTx 60 vhash txId2
+    case maybeTxin of
+      Just txin -> do
+        logInfo' "Try to increment"
+        txId2 <- incHello 6 txin vhash validator
 
-        logInfo' "Try to spend"
-        spendFromHello vhash validator txId2
+        maybeTxIn2 <- waitForTx 60 vhash txId2
+        case maybeTxIn2 of
+          Just txIn2 -> do
+            logInfo' "Try to spend"
+            txId3 <- spendFromHello txIn2 vhash validator
+            logInfo' $ "finished with txid:" <> show txId3
+          Nothing -> logInfo' "failed"
       Nothing -> logInfo' "failed"
 
 -- TODO it would probably be better to make this return the txInput
-countToZeroOrTx :: Int -> ValidatorHash -> TransactionHash -> Contract () Unit
-countToZeroOrTx n vhash txid = do
+waitForTx :: Int -> ValidatorHash -> TransactionHash -> Contract () (Maybe TransactionInput)
+waitForTx n vhash txid = do
   let scriptAddress = scriptHashAddress vhash
   UtxoM utxos <- fromMaybe (UtxoM Map.empty) <$> utxosAt scriptAddress
   case fst <$> find hasTransactionId (Map.toUnfoldable utxos :: Array (TransactionInput /\ TransactionOutput)) of
-      Nothing -> unless (n <= 0) $ do
-        logInfo' $ "No tx yet wating: " <> show n
-        (liftAff <<< delay <<< wrap) 1000.0
-        countToZeroOrTx (n - 1) vhash txid
-      Just _ -> logInfo' $ "found tx stoping wait at: " <> show n
+      Nothing ->
+        if (n <= 0)
+          then do
+            pure Nothing
+          else do
+            logInfo' $ "No tx yet, waiting for: " <> show n <> " more seconds"
+            (liftAff <<< delay <<< wrap) 1000.0
+            waitForTx (n - 1) vhash txid
+      Just txin -> do
+        logInfo' $ "found tx:" <> show txid
+        pure $ Just txin
   where
     hasTransactionId :: forall a. TransactionInput /\ a -> Boolean
     hasTransactionId (TransactionInput tx /\ _) =
@@ -106,70 +118,44 @@ payToHello n vhash = do
 
 incHello
   :: Int
+  -> TransactionInput
   -> ValidatorHash
   -> Validator
-  -> TransactionHash
-  -> Contract () (Maybe TransactionHash)
-incHello n vhash validator txId = do
+  -> Contract () TransactionHash
+incHello n txInput vhash validator = do
   let scriptAddress = scriptHashAddress vhash
   UtxoM utxos <- fromMaybe (UtxoM Map.empty) <$> utxosAt scriptAddress
-  case fst <$> find hasTransactionId (Map.toUnfoldable utxos :: Array _) of
-    Just txInput ->
-      let
-        lookups :: Lookups.ScriptLookups PlutusData
-        lookups = Lookups.validator validator
-          <> Lookups.unspentOutputs utxos
+  let
+    lookups :: Lookups.ScriptLookups PlutusData
+    lookups = Lookups.validator validator
+        <> Lookups.unspentOutputs utxos
+    constraints :: TxConstraints Unit Unit
+    constraints =
+      (Constraints.mustSpendScriptOutput txInput unitRedeemer)
+      <>
+      (Constraints.mustPayToScript vhash (Datum $ toData (BigInt.fromInt n :: BigInt.BigInt))
+        $ Value.lovelaceValueOf
+        $ BigInt.fromInt 2_000_000
+      )
 
-        constraints :: TxConstraints Unit Unit
-        constraints =
-          (Constraints.mustSpendScriptOutput txInput unitRedeemer)
-          <>
-          (Constraints.mustPayToScript vhash (Datum $ toData (BigInt.fromInt n :: BigInt.BigInt))
-            $ Value.lovelaceValueOf
-            $ BigInt.fromInt 2_000_000
-          )
-
-      in
-        Just <$> buildBalanceSignAndSubmitTx lookups constraints
-    _ -> do
-      logInfo' $ "The id "
-        <> show txId
-        <> " does not have output locked at: "
-        <> show scriptAddress
-      pure Nothing
-  where
-  hasTransactionId :: TransactionInput /\ _ -> Boolean
-  hasTransactionId (TransactionInput tx /\ _) =
-    tx.transactionId == txId
+  buildBalanceSignAndSubmitTx lookups constraints
 
 spendFromHello
-  :: ValidatorHash
+  :: TransactionInput
+  -> ValidatorHash
   -> Validator
-  -> TransactionHash
-  -> Contract () Unit
-spendFromHello vhash validator txId = do
+  -> Contract () TransactionHash
+spendFromHello txInput vhash validator = do
   let scriptAddress = scriptHashAddress vhash
   UtxoM utxos <- fromMaybe (UtxoM Map.empty) <$> utxosAt scriptAddress
-  case fst <$> find hasTransactionId (Map.toUnfoldable utxos :: Array _) of
-    Just txInput ->
-      let
-        lookups :: Lookups.ScriptLookups PlutusData
-        lookups = Lookups.validator validator
-          <> Lookups.unspentOutputs utxos
+  let
+    lookups :: Lookups.ScriptLookups PlutusData
+    lookups = Lookups.validator validator
+      <> Lookups.unspentOutputs utxos
 
-        constraints :: TxConstraints Unit Unit
-        constraints = Constraints.mustSpendScriptOutput txInput spendRedeemer
-      in
-        void $ buildBalanceSignAndSubmitTx lookups constraints
-    _ ->
-      logInfo' $ "The id "
-        <> show txId
-        <> " does not have output locked at: "
-        <> show scriptAddress
-  where
-  hasTransactionId :: TransactionInput /\ _ -> Boolean
-  hasTransactionId (TransactionInput tx /\ _) =
-    tx.transactionId == txId
+    constraints :: TxConstraints Unit Unit
+    constraints = Constraints.mustSpendScriptOutput txInput spendRedeemer
+  buildBalanceSignAndSubmitTx lookups constraints
 
 buildBalanceSignAndSubmitTx
   :: Lookups.ScriptLookups PlutusData
