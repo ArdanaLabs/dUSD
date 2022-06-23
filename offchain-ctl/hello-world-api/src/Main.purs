314e2356549da3b5c2507502e4098c7cdac6af16
Geometer1729 <16kuhnb@gmail.com>
2022-06-23 14:24:44 -0400

    start cli stuff

--- offchain-ctl/hello-world-api/src/Main.purs
+++ offchain-ctl/hello-world-api/src/Main.purs
@@ -4,16 +4,30 @@ module Main
 
 import Contract.Prelude
 import UnitTest (helloUnitTest)
-import Contract.Wallet (mkNamiWalletAff)
 import Contract.Monad
-  ( ContractConfig(ContractConfig)
-  , launchAff_
+  ( launchAff_
   , runContract_
-  , traceTestnetContractConfig
+  , configWithLogLevel
   )
+import Contract.Wallet.KeyFile(mkKeyWalletFromFile)
+import Serialization.Address (NetworkId(TestnetId))
+import Data.Log.Level (LogLevel(Trace))
+
+
+
+--import Wallet(mkNamiWalletAff)
+--import Wallet(mkKeyWalletFromFile)
+--import Wallet.KeyFile (privateKeyFromFile)
+--import Wallet(mkKeyWallet)
 
 main :: Effect Unit
 main = launchAff_ $ do
-  wallet <- Just <$> mkNamiWalletAff
-  cfg <- over ContractConfig _ { wallet = wallet } <$> traceTestnetContractConfig
-  runContract_ cfg helloUnitTest
+  wallet' <- mkKeyWalletFromFile "wallet.skey"
+  --wallet <- Just <$> mkNamiWalletAff
+  --wallet <- map mkKeyWallet <$> privateKeyFromFile "./key"
+  case wallet' of
+    Just wallet -> do
+      cfg <- configWithLogLevel TestnetId wallet Trace
+      --cfg <- over ContractConfig _ { wallet = wallet } <$> traceTestnetContractConfig
+      runContract_ cfg helloUnitTest
+    Nothing -> undefined
