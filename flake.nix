2bf4b48954192d52950f7185ac39280b672f4af4
Geometer1729 <16kuhnb@gmail.com>
2022-06-27 14:36:51 -0400

    update ctl

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     cardano-node.url = "github:input-output-hk/cardano-node?rev=73f9a746362695dc2cb63ba757fbcabb81733d23";
-    cardano-transaction-lib.url = "github:Plutonomicon/cardano-transaction-lib?rev=6d47a100781379b54debc801b4f13a21ea182c23";
+    cardano-transaction-lib.url = "github:Plutonomicon/cardano-transaction-lib?rev=6fca5ee2ff27f81762689c7da0d307e1afe7bbd3";
     #   used for libsodium-vrf
     plutus.url = "github:input-output-hk/plutus";
     plutus-apps.url = "github:input-output-hk/plutus-apps?rev=e4062bca213f233cdf9822833b07aa69dff6d22a";
@@ -27,8 +27,8 @@
       flake = false;
       url = "github:nix-community/npmlock2nix";
     };
-    purs-nix.url = "github:ursi/purs-nix";
-    purs-nix-0-14.url = "github:ursi/purs-nix/ps-0.14";
+    purs-nix.url = "github:Geometer1729/purs-nix";
+    purs-nix-0-14.url = "github:Geometer1729/purs-nix?rev=3fafe63040257b2740e7dfe5db311f16cbf89fd0";
   };
 
   outputs = { self, flake-modules-core, ... }:
