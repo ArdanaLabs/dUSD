ca299a90b64f5002f1af2a5761ce8aaf63ae7543
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-07-01 19:26:01 +0300

    build(nix): refactor offchain-ctl flake modules
    - refactor the mostly duplicate devshells into a common shell
    and use overrideAttrs to create specific shells
    - minor style improvements

--- offchain-ctl/flake-module.nix
+++ offchain-ctl/flake-module.nix
@@ -12,23 +12,23 @@
 
     ps-pkgs-ctl = let
       f = self:
-        import ./ps-pkgs-ctl.nix {ps-pkgs = purs-nix.ps-pkgs // self;};
+        import ./ps-pkgs-ctl.nix
+        {ps-pkgs = purs-nix.ps-pkgs // self;};
     in
-      pkgs.lib.fix
-      (
+      pkgs.lib.fix (
         self:
-          builtins.mapAttrs (n: v: purs-nix.build (v // {name = n;})) (f self)
+          builtins.mapAttrs
+          (n: v: purs-nix.build (v // {name = n;}))
+          (f self)
       );
     all-ps-pkgs = purs-nix.ps-pkgs // ps-pkgs-ctl;
 
-    hello-world-cbor =
-      purs-nix.build
-      {
-        name = "hello-world-cbor";
-        src.path = self'.packages.hello-world-cbor-purs;
-        info.dependencies = [];
-        info.version = "0.0.1";
-      };
+    hello-world-cbor = purs-nix.build {
+      name = "hello-world-cbor";
+      src.path = self'.packages.hello-world-cbor-purs;
+      info.dependencies = [];
+      info.version = "0.0.1";
+    };
 
     hello-world-api = {
       dependencies = with all-ps-pkgs; [
@@ -40,46 +40,38 @@
         ordered-collections
         spec
       ];
-      ps =
-        purs-nix.purs
-        {
+      ps = purs-nix.purs {
+        inherit (hello-world-api) dependencies;
+        srcs = [./hello-world-api/src];
+      };
+      package = purs-nix.build {
+        name = "hello-world-api";
+        src.path = ./hello-world-api;
+        info = {
           inherit (hello-world-api) dependencies;
-          srcs = [./hello-world-api/src];
+          version = "0.0.1";
         };
-      package =
-        purs-nix.build
-        {
-          name = "hello-world-api";
-          src.path = ./hello-world-api;
-          info = {
-            inherit (hello-world-api) dependencies;
-            version = "0.0.1";
-          };
-        };
+      };
     };
 
     hello-world-browser = {
-      ps =
-        purs-nix.purs
-        {
-          dependencies = with all-ps-pkgs; [
-            cardano-transaction-lib
-            hello-world-api.package
-          ];
-          srcs = [./hello-world-browser/src];
-        };
+      ps = purs-nix.purs {
+        dependencies = with all-ps-pkgs; [
+          cardano-transaction-lib
+          hello-world-api.package
+        ];
+        srcs = [./hello-world-browser/src];
+      };
     };
 
     hello-world-cli = {
-      ps =
-        purs-nix.purs
-        {
-          dependencies = with all-ps-pkgs; [
-            prelude
-            hello-world-api.package
-          ];
-          srcs = [./hello-world-cli/src];
-        };
+      ps = purs-nix.purs {
+        dependencies = with all-ps-pkgs; [
+          prelude
+          hello-world-api.package
+        ];
+        srcs = [./hello-world-cli/src];
+      };
     };
 
     ctl-pkgs = import self.inputs.nixpkgs {
@@ -94,79 +86,67 @@
       };
     };
   in {
-    packages.hello-world-cbor = hello-world-cbor;
-
-    packages.hello-world-api = hello-world-api.package;
-
-    packages.hello-world-browser =
-      pkgs.runCommand "build-hello-world-browser" {}
-      # see buildPursProjcet: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L74
-      # see bundlePursProject: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L149
-      ''
-        mkdir $out; cd $out
-        export BROWSER_RUNTIME=1
-        cp -r ${hello-world-browser.ps.modules.Main.output {}} output
-        cp ${./hello-world-browser/index.js} index.js
-        cp ${./hello-world-browser/index.html} index.html
-        cp ${./webpack.config.js} webpack.config.js
-        cp -r ${npmlock2nix.node_modules {src = ./.;}}/* .
-        export NODE_PATH="node_modules"
-        export PATH="bin:$PATH"
-        mkdir dist
-        webpack --mode=production -c webpack.config.js -o ./dist --entry ./index.js
-      '';
-
-    packages.hello-world-cli = let
-      js = "${hello-world-cli.ps.modules.Main.output {}}/Main/index.js";
-    in
-      pkgs.writeScriptBin "hello-world-cli"
-      ''
-        export NODE_PATH=${npmlock2nix.node_modules {src = ./.;}}/node_modules
-        echo 'require("${js}").main()' | ${pkgs.nodejs}/bin/node
-      '';
+    packages = {
+      hello-world-cbor = hello-world-cbor;
+      hello-world-api = hello-world-api.package;
+      hello-world-browser =
+        pkgs.runCommand "build-hello-world-browser" {}
+        # see buildPursProjcet: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L74
+        # see bundlePursProject: https://github.com/Plutonomicon/cardano-transaction-lib/blob/c906ead97563fef3b554320bb321afc31956a17e/nix/default.nix#L149
+        ''
+          mkdir $out; cd $out
+          export BROWSER_RUNTIME=1
+          cp -r ${hello-world-browser.ps.modules.Main.output {}} output
+          cp ${./hello-world-browser/index.js} index.js
+          cp ${./hello-world-browser/index.html} index.html
+          cp ${./webpack.config.js} webpack.config.js
+          cp -r ${npmlock2nix.node_modules {src = ./.;}}/* .
+          export NODE_PATH="node_modules"
+          export PATH="bin:$PATH"
+          mkdir dist
+          webpack --mode=production -c webpack.config.js -o ./dist --entry ./index.js
+        '';
+      hello-world-cli = let
+        js = "${hello-world-cli.ps.modules.Main.output {}}/Main/index.js";
+      in
+        pkgs.writeShellScriptBin "hello-world-cli" ''
+          export NODE_PATH=${npmlock2nix.node_modules {src = ./.;}}/node_modules
+          echo 'require("${js}").main()' | ${pkgs.nodejs}/bin/node
+        '';
+    };
 
     apps = {
       ctl-runtime = ctl-pkgs.launchCtlRuntime config;
-
       serve-hello-world-browser = {
         type = "app";
-        program =
-          pkgs.writeShellApplication
-          {
-            name = projectName;
-            runtimeInputs = [
-              pkgs.nodePackages.http-server
-            ];
-            text = "http-server -c-1 ${self'.packages.hello-world-browser}";
-          };
+        program = pkgs.writeShellApplication {
+          name = projectName;
+          runtimeInputs = [pkgs.nodePackages.http-server];
+          text = "http-server -c-1 ${self'.packages.hello-world-browser}";
+        };
       };
     };
 
-    devShells.hello-world-browser = pkgs.mkShell {
-      name = projectName;
-      buildInputs = with pkgs; [
-        nodejs-16_x
-        (hello-world-browser.ps.command {})
-        purs-nix.ps-pkgs.psci-support
-        purs-nix.purescript
-        purs-nix.purescript-language-server
-        nodePackages.purs-tidy
-      ];
-      shellHook = "export NODE_PATH=${npmlock2nix.node_modules {src = ./.;}}/node_modules/";
-    };
-    devShells.hello-world-api = pkgs.mkShell {
-      name = projectName;
-      buildInputs = with pkgs; [
-        nodejs-16_x
-        (hello-world-api.ps.command {})
-        purs-nix.ps-pkgs.psci-support
-        purs-nix.purescript
-        purs-nix.purescript-language-server
-        nodePackages.purs-tidy
-      ];
-      shellHook = "export NODE_PATH=${npmlock2nix.node_modules {src = ./.;}}/node_modules/";
+    devShells = let
+      commonHelloWorldShell = pkgs.mkShell {
+        name = projectName;
+        buildInputs = with pkgs; [
+          nodejs-16_x
+          purs-nix.ps-pkgs.psci-support
+          purs-nix.purescript
+          purs-nix.purescript-language-server
+          nodePackages.purs-tidy
+        ];
+        NODE_PATH = "${npmlock2nix.node_modules {src = ./.;}}/node_modules/";
+      };
+    in {
+      hello-world-browser = commonHelloWorldShell.overrideAttrs (old: {
+        buildInputs = old.buildInputs ++ [(hello-world-browser.ps.command {})];
+      });
+      hello-world-api = commonHelloWorldShell.overrideAttrs (old: {
+        buildInputs = old.buildInputs ++ [(hello-world-api.ps.command {})];
+      });
     };
   };
-  flake = {
-  };
+  flake = {};
 }
