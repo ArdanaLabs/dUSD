782bbb87d6294220daf828f3a81414d7a61d8da7
David Wilson <30732161+Anteproperispomenon@users.noreply.github.com>
2022-07-21 14:26:24 -0400

    Added lint flake.

---
+++ nix/flake-modules/lint/flake-module.nix
@@ -0,0 +1,44 @@
+{ self, ... }:
+{
+  perSystem = { config, self', inputs', system, ... }:
+    let
+      # TODO: We should be able to get rid of this once
+      # https://github.com/numtide/treefmt/issues/165 is implemented upstream.
+      # Uses https://github.com/numtide/treefmt to provide auto-formating for multiple
+      # languages. Configure the behaviour in treefmt.toml at project root.
+      pkgs = inputs'.nixpkgs.legacyPackages;
+      dependencies = with pkgs; [
+        haskellPackages.hlint
+        # haskellPackages.fourmolu
+        # treefmt
+        # nixpkgs-fmt
+        nodePackages.purs-tidy
+      ];
+    in
+    {
+      apps = {
+        # Formats the project tree.
+        lint = {
+          type = "app";
+          program = pkgs.writeShellApplication {
+            name = "lint";
+            runtimeInputs = dependencies;
+            text = "hlint .";
+          };
+        };
+      };
+      checks = {
+        # Checks that the project tree is *already* formatted.
+        lint = pkgs.runCommandLocal "lint-check"
+          {
+            buildInputs = dependencies;
+          } ''
+          # set -e
+          # export HOME="$TMP"
+          # treefmt -vvv --no-cache --fail-on-change -C ${self}
+          # touch $out
+        '';
+      };
+    };
+  flake = { };
+}
