e20068c70b3b96cdd9fe15d24b398ec6c8c20ef2
Geometer1729 <16kuhnb@gmail.com>
2022-07-25 16:54:19 -0400

    this might work

--- offchain/hello-world-browser/src/Main.purs
+++ offchain/hello-world-browser/src/Main.purs
@@ -3,24 +3,38 @@ module Main
   , testMain
   ) where
 
-import Prelude
+import Contract.Prelude
 
-import Contract.Monad (defaultTestnetContractConfig)
-import Data.Maybe (Maybe(Nothing))
+import Contract.Monad (configWithLogLevel)
+import Data.Maybe (Maybe(Nothing,Just))
+import Data.Log.Level (LogLevel(Trace))
 import Effect (Effect)
+import Effect.Exception(throw)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import HelloWorld.AppM (runAppM)
 import HelloWorld.Page.Home as Home
 import HelloWorld.TestM (runTestM)
+import Serialization (privateKeyFromBytes)
+import Serialization.Address (NetworkId(TestnetId))
+import Wallet(mkKeyWallet,Wallet)
+import Wallet.Key
+  ( PrivatePaymentKey(PrivatePaymentKey)
+  , PrivateStakeKey(PrivateStakeKey)
+  )
+import Types.RawBytes(hexToRawBytes)
+
+
 
 main :: Effect Unit
 main =
   HA.runHalogenAff do
     body <- HA.awaitBody
-
-    contractConfig <- defaultTestnetContractConfig
+    wallet <- case keyWallet of
+      Nothing -> liftEffect $ throw "key wallet failed to decode"
+      Just w -> pure w
+    contractConfig <- configWithLogLevel TestnetId wallet Trace
     let
       store =
         { contractConfig
@@ -28,6 +42,12 @@ main =
         }
     rootComponent <- runAppM store Home.component
     runUI rootComponent unit body
+
+keyWallet :: Maybe Wallet
+keyWallet = do
+  privateKey <- PrivatePaymentKey <$> (privateKeyFromBytes =<< (hexToRawBytes "5820d071b5fc9e6f8d1cbc0a4b22dd2ce4fb8f537d0bfe3e6073a758e65c1591275e"))
+  stakingKey <- PrivateStakeKey <$> (privateKeyFromBytes =<< (hexToRawBytes "58204b43bbce308317030a526ae92f9579ecafebaa0da20fa46ddc48e12e07de6472"))
+  pure $ mkKeyWallet privateKey (Just stakingKey)
 
 testMain :: Effect Unit
 testMain =
