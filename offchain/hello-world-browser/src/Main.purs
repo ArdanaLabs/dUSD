f37b6badc344ed22c65b6426e393994aec8f0e03
Hao <hao.liu@platonic.systems>
2022-07-27 15:13:57 +1200

    Drop TestM

--- offchain/hello-world-browser/src/Main.purs
+++ offchain/hello-world-browser/src/Main.purs
@@ -5,39 +5,26 @@ module Main
 
 import Contract.Prelude
 
-import Contract.Monad (configWithLogLevel)
-import Cardano.TextEnvelope
-  ( TextEnvelopeType
-      ( PaymentSigningKeyShelleyed25519
-      , StakeSigningKeyShelleyed25519
-      )
-  , textEnvelopeBytes
-  )
-import Data.Maybe (Maybe(Nothing,Just))
+import Cardano.TextEnvelope (TextEnvelopeType(PaymentSigningKeyShelleyed25519, StakeSigningKeyShelleyed25519), textEnvelopeBytes)
+import Contract.Monad (configWithLogLevel, defaultTestnetContractConfig)
 import Data.Log.Level (LogLevel(Trace))
 import Data.Newtype (wrap)
 import Effect (Effect)
-import Effect.Exception(error)
-import Halogen as H
+import Effect.Exception (error)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import HelloWorld.AppM (runAppM)
 import HelloWorld.Page.Home as Home
-import HelloWorld.TestM (runTestM)
 import Serialization (privateKeyFromBytes)
 import Serialization.Address (NetworkId(TestnetId))
-import Wallet(mkKeyWallet,mkNamiWalletAff,Wallet)
-import Wallet.Key
-  ( PrivatePaymentKey(PrivatePaymentKey)
-  , PrivateStakeKey(PrivateStakeKey)
-  )
+import Wallet (mkKeyWallet, Wallet)
+import Wallet.Key (PrivatePaymentKey(PrivatePaymentKey), PrivateStakeKey(PrivateStakeKey))
 
 main :: Effect Unit
 main =
   HA.runHalogenAff do
     body <- HA.awaitBody
-    wallet <- mkNamiWalletAff
-    contractConfig <- configWithLogLevel TestnetId wallet Trace
+    contractConfig <- defaultTestnetContractConfig
     let
       store =
         { contractConfig
@@ -62,13 +49,15 @@ testMain =
 
 loadKeyWallet :: Aff Wallet
 loadKeyWallet = do
-  let keyStr   = "{\"type\":\"PaymentSigningKeyShelley_ed25519\",\"description\":\"PaymentSigningKey\",\"cborHex\":\"5820d071b5fc9e6f8d1cbc0a4b22dd2ce4fb8f537d0bfe3e6073a758e65c1591275e\"}"
+  let keyStr = "{\"type\":\"PaymentSigningKeyShelley_ed25519\",\"description\":\"PaymentSigningKey\",\"cborHex\":\"5820d071b5fc9e6f8d1cbc0a4b22dd2ce4fb8f537d0bfe3e6073a758e65c1591275e\"}"
   let stakeStr = "{\"type\":\"StakeSigningKeyShelley_ed25519\",\"description\":\"StakeSigningKey\",\"cborHex\":\"58204b43bbce308317030a526ae92f9579ecafebaa0da20fa46ddc48e12e07de6472\"}"
   keyBytes <- textEnvelopeBytes keyStr PaymentSigningKeyShelleyed25519
   stakeBytes <- textEnvelopeBytes stakeStr StakeSigningKeyShelleyed25519
   privateKey <- liftM (error "Unable to decode private payment key")
-    $ PrivatePaymentKey <$> privateKeyFromBytes (wrap keyBytes)
+    $ PrivatePaymentKey
+    <$> privateKeyFromBytes (wrap keyBytes)
   stakingKey <- liftM (error "Unable to decode private stake key")
-    $ PrivateStakeKey <$> privateKeyFromBytes (wrap stakeBytes)
+    $ PrivateStakeKey
+    <$> privateKeyFromBytes (wrap stakeBytes)
   pure $ mkKeyWallet privateKey (Just stakingKey)
 
