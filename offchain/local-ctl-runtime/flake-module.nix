783ec3427536bc94a329b734f6ff850db42187c1
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-08-19 20:10:35 +0200

    flake/local-ctl-runtime: add more cli parameters

--- offchain/local-ctl-runtime/flake-module.nix
+++ offchain/local-ctl-runtime/flake-module.nix
@@ -19,6 +19,7 @@
             effect
             optparse
             prelude
+            tailrec
             uint
           ];
         ps =
--- offchain/local-ctl-runtime/src/Main.purs
+++ offchain/local-ctl-runtime/src/Main.purs
@@ -1,78 +1,99 @@
 module Main (main) where
 
-import Contract.Prelude (LogLevel(Error))
+import Prelude
+
+import Contract.Prelude (LogLevel(..), liftEffect)
 import Contract.Test.Plutip (withPlutipContractEnv, PlutipConfig)
-import Data.Generic.Rep (class Generic)
-import Data.Show.Generic (genericShow)
+import Control.Monad.Rec.Class (forever)
 import Data.BigInt as BigInt
-import Data.UInt as UInt
+import Data.Generic.Rep (class Generic)
+import Data.Log.Level (LogLevel(Error, Info))
 import Data.Maybe (Maybe(Nothing), fromMaybe)
+import Data.String (toLower)
+import Data.UInt as UInt
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Effect.Console (logShow)
-import Effect.Class (liftEffect)
-import Options.Applicative (Parser, ParserInfo, execParser, fullDesc, header, help, helper, info, int, long, metavar, option, progDesc, short, value, (<**>))
+import Effect.Console (log, error)
+import Options.Applicative (Parser, ParserInfo, execParser, fullDesc, header, help, helper, info, int, long, metavar, option, str, progDesc, short, value, (<**>))
 import Wallet.Key (keyWalletPrivatePaymentKey)
 import Wallet.KeyFile (formatPaymentKey)
 
-import Prelude
+-- add debug level options
+-- add port options
 
 main :: Effect Unit
 main =
-  execParser opts >>= \(LocalCtlRuntimeOptions initialAdaValue) -> do
-    launchAff_ $ withPlutipContractEnv config [ initialAdaValue ] \_ wallets -> do
-        let key = (fromMaybe "Couldn't format the private payment key" <<< formatPaymentKey <<< keyWalletPrivatePaymentKey) $ wallets
-        liftEffect $ logShow key
+  execParser opts >>= \(LocalCtlRuntimeOptions lo) -> do
+    launchAff_ $ withPlutipContractEnv (makeConfig $ LocalCtlRuntimeOptions lo) [ lo.initialAdaAmount ] \_ wallets -> do
+        let key = (fromMaybe "Unable to format the received private payment key" <<< formatPaymentKey <<< keyWalletPrivatePaymentKey) $ wallets
+        liftEffect <<< error $ "All CTL runtime dependencies started successfully."
+        liftEffect <<< error $ "Private payment key funded with " <> BigInt.toString lo.initialAdaAmount
+        liftEffect <<< log $ key
+        liftEffect <<< error $ "Press Ctrl-C to stop the runtime"
+        forever $ pure unit
 
-config :: PlutipConfig
-config =
+makeConfig :: LocalCtlRuntimeOptions -> PlutipConfig
+makeConfig (LocalCtlRuntimeOptions options) =
   { host: "127.0.0.1"
-  , port: UInt.fromInt 8082
-  , logLevel: Error
+  , port: (options.plutipServerPort)
+  , logLevel: options.plutipLogLevel
   -- Server configs are used to deploy the corresponding services.
   , ogmiosConfig:
-      { port: UInt.fromInt 1338
+      { port: options.ogmiosPort
       , host: "127.0.0.1"
       , secure: false
       , path: Nothing
       }
   , ogmiosDatumCacheConfig:
-      { port: UInt.fromInt 10000
+      { port: options.ogmiosDatumCachePort
       , host: "127.0.0.1"
       , secure: false
       , path: Nothing
       }
   , ctlServerConfig:
-      { port: UInt.fromInt 8083
+      { port: options.ctlServerPort
       , host: "127.0.0.1"
       , secure: false
       , path: Nothing
       }
   , postgresConfig:
       { host: "127.0.0.1"
-      , port: UInt.fromInt 5433
+      , port: options.postgresPort
       , user: "ctxlib"
       , password: "ctxlib"
       , dbname: "ctxlib"
       }
   }
 
-data LocalCtlRuntimeOptions = LocalCtlRuntimeOptions BigInt.BigInt
-
+newtype LocalCtlRuntimeOptions = LocalCtlRuntimeOptions {
+  initialAdaAmount :: BigInt.BigInt
+, plutipLogLevel :: LogLevel
+, plutipServerPort :: UInt.UInt
+, ogmiosPort :: UInt.UInt
+, ogmiosDatumCachePort :: UInt.UInt
+, ctlServerPort :: UInt.UInt
+, postgresPort :: UInt.UInt
+}
 derive instance genericLocalRuntimeOptions :: Generic LocalCtlRuntimeOptions _
-instance showLocalRuntimeOptions :: Show LocalCtlRuntimeOptions where
-  show = genericShow
 
 opts :: ParserInfo LocalCtlRuntimeOptions
 opts = 
   info
-    (localCtlRuntimeOptions <**> helper)
+    (localCtlRuntimeOptionsParser <**> helper)
     (fullDesc
     <> progDesc "Run the CTL runtime with a local testnet (enabled by plutip)"
     <> header "local-ctl-runtime")
 
-localCtlRuntimeOptions :: Parser LocalCtlRuntimeOptions
-localCtlRuntimeOptions = LocalCtlRuntimeOptions <$> initialAdaValueParser
+localCtlRuntimeOptionsParser :: Parser LocalCtlRuntimeOptions
+localCtlRuntimeOptionsParser =
+  map LocalCtlRuntimeOptions $ { initialAdaAmount: _, plutipLogLevel: _, plutipServerPort: _, ogmiosPort: _, ogmiosDatumCachePort: _, ctlServerPort: _, postgresPort: _}
+  <$> initialAdaValueParser
+  <*> plutipLogLevelParser
+  <*> makePortParser "plutip-server" (UInt.fromInt 8082)
+  <*> makePortParser "ogmios" (UInt.fromInt 1338)
+  <*> makePortParser "odc" (UInt.fromInt 10000)
+  <*> makePortParser "ctl-server" (UInt.fromInt 8083)
+  <*> makePortParser "postgres" (UInt.fromInt 5433)
 
 initialAdaValueParser :: Parser BigInt.BigInt
 initialAdaValueParser = option (BigInt.fromInt <$> int)
@@ -81,3 +102,25 @@ initialAdaValueParser = option (BigInt.fromInt <$> int)
     <> metavar "INT"
     <> value (BigInt.fromInt 100_000_000)
     <> help "The initial funding in lovelace for the test wallet")
+
+plutipLogLevelParser :: Parser LogLevel
+plutipLogLevelParser = option (logLevelFromString <$> str) (
+     long "log-level"
+  <> short 'l'
+  <> metavar "LOG_LEVEL"
+  <> value Info
+  <> help "Plutip log-level")
+
+logLevelFromString :: String -> LogLevel
+logLevelFromString level
+  | toLower level ==  "error" = Error
+  | toLower level ==  "info" = Info
+  | toLower level ==  "debug" = Debug
+  | otherwise = Info
+
+makePortParser :: String -> UInt.UInt -> Parser UInt.UInt
+makePortParser name port = option (UInt.fromInt <$> int)
+  (  long (name <> "-port")
+  <> metavar "PORT"
+  <> value port
+  <> help ("The port " <> name <> " should listen on"))
